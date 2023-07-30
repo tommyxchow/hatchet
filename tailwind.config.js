1d38ba6db045e8a8168cc7dc76fd2b7ce55f7276
Tommy Chow <tommyxchow@gmail.com>
2023-07-29 20:43:45 -0400

    Setup prettier and  install zod

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+    },
   },
   plugins: [],
 };
