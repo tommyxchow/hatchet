3b65a34d6f8306da0f20152f6ac9ea53a33c1e7f
Tommy Chow <tommyxchow@gmail.com>
2023-11-26 16:15:42 -0500

    Make comment links blue

--- src/components/ui/ItemText.tsx
+++ src/components/ui/ItemText.tsx
@@ -5,7 +5,7 @@ interface ItemTextProps {
 export function ItemText({ text }: ItemTextProps) {
   return (
     <div
-      className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200 prose-a:break-all'
+      className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200 prose-a:break-all prose-a:font-normal prose-a:text-blue-400'
       dangerouslySetInnerHTML={{ __html: text }}
     />
   );
