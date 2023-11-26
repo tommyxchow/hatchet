b6c73020e6dfc74c1036de332734193316684a37
Tommy Chow <tommyxchow@gmail.com>
2023-11-26 15:01:15 -0500

    Fix links causing overflow

--- src/components/ui/ItemText.tsx
+++ src/components/ui/ItemText.tsx
@@ -5,7 +5,7 @@ interface ItemTextProps {
 export function ItemText({ text }: ItemTextProps) {
   return (
     <div
-      className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200'
+      className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200 prose-a:break-all'
       dangerouslySetInnerHTML={{ __html: text }}
     />
   );
