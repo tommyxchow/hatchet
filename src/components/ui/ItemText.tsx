3e9c0f20a2e3a6a882b5344a313b35477f3358c0
Tommy Chow <tommyxchow@gmail.com>
2023-11-26 20:29:57 -0500

    Fix pre/code blocks causing overflow

--- src/components/ui/ItemText.tsx
+++ src/components/ui/ItemText.tsx
@@ -5,7 +5,7 @@ interface ItemTextProps {
 export function ItemText({ text }: ItemTextProps) {
   return (
     <div
-      className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200 prose-a:break-all prose-a:font-normal prose-a:text-blue-400'
+      className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200 prose-a:break-all prose-a:font-normal prose-a:text-blue-400 prose-pre:whitespace-pre-wrap prose-pre:px-0 prose-pre:[overflow-wrap:anywhere]'
       dangerouslySetInnerHTML={{ __html: text }}
     />
   );
