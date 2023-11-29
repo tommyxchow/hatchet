1052c43d893807ad90f4f63f8528095a1e05dd22
Tommy Chow <tommyxchow@gmail.com>
2023-11-29 04:11:28 -0500

    Fix first text child formatting

--- src/components/ui/ItemText.tsx
+++ src/components/ui/ItemText.tsx
@@ -4,9 +4,11 @@ interface ItemTextProps {
 
 export function ItemText({ text }: ItemTextProps) {
   return (
-    <div
-      className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200 prose-a:break-all prose-a:font-normal prose-a:text-blue-400 prose-pre:whitespace-pre-wrap prose-pre:px-0 prose-pre:[overflow-wrap:anywhere]'
-      dangerouslySetInnerHTML={{ __html: text }}
-    />
+    <div className='prose prose-neutral prose-invert max-w-screen-md text-neutral-200 prose-a:break-all prose-a:font-normal prose-a:text-blue-400 prose-pre:whitespace-pre-wrap prose-pre:px-0 prose-pre:[overflow-wrap:anywhere]'>
+      <span
+        className='[&_*:last-child]:mb-0'
+        dangerouslySetInnerHTML={{ __html: text }}
+      />
+    </div>
   );
 }
