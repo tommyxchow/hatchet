9f1c44ef79d14cbb5c7337d8c8a387ceb5e66c19
Tommy Chow <tommyxchow@gmail.com>
2024-07-23 20:21:27 -0700

    Set up TanStack Query Devtools

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
 
 interface ProvidersProps {
@@ -13,6 +14,7 @@ export function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute='class'>{children}</ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
