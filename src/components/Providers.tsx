3626e670e6b35f6e3c7ac5bea9bd78abe4878367
Tommy Chow <tommyxchow@gmail.com>
2024-07-23 20:19:11 -0700

    Restore `refetchOnWindowFocus`

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -7,15 +7,9 @@ interface ProvidersProps {
   children: React.ReactNode;
 }
 
-export function Providers({ children }: ProvidersProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
+const queryClient = new QueryClient();
 
+export function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute='class'>{children}</ThemeProvider>
