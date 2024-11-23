8531b06cc0ced5c17bc1aad1c0e470a34c20a316
Tommy Chow <tommyxchow@gmail.com>
2024-11-23 16:03:26 -0500

    move `queryClient`

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,19 +3,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 
-interface ProvidersProps {
-  children: React.ReactNode;
-}
-
-export function Providers({ children }: ProvidersProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
     },
-  });
+  },
+});
 
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute='class'>{children}</ThemeProvider>
