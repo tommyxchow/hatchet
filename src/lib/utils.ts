2be6901b3c12d0310d5e905b8d0f329588a5f3a6
Tommy Chow <tommyxchow@gmail.com>
2025-01-27 21:57:28 -0800

    Revert "restore cache for thumbnail fetching"
    This reverts commit 313e769fc36da93629540544f3a2418cfd4845a9.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -36,7 +36,9 @@ export function getDisplayURL(url: string): string {
 
 export async function getThumbnailUrl(url: string): Promise<string | null> {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      cache: 'no-store',
+    });
 
     const contentType = response.headers.get('content-type');
     if (!contentType?.includes('text/html')) {
